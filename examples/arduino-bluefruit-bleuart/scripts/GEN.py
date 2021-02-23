65201b3494fc684222f4e1166abe4f4e08b4b9fc
qitas <qitas@qitas.cn>
2021-02-23 17:16:50 +0800

    fix

--- examples/arduino-bluefruit-bleuart/scripts/GEN.py
+++ examples/arduino-bluefruit-bleuart/scripts/GEN.py
@@ -3,8 +3,8 @@ import shutil
 import os
 
 def after_build(source, target, env):
-	shutil.copy(firmware_source, './firmware.hex')
+	shutil.copy(firmware_source, './firmware.bin')
 
 env.AddPostAction("buildprog", after_build)
 
-firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware.hex")
+firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware_signature.bin")
