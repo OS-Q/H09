46932c21b697f89a61360a5327359ea00c6afaec
qitas <qitas@qitas.cn>
2021-02-24 14:57:08 +0800

    fix

--- examples/arduino-serial-plotter/scripts/firmware.py
+++ examples/arduino-serial-plotter/scripts/firmware.py
@@ -2,12 +2,13 @@ Import("env")
 import shutil
 import os
 
+firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware.bin")
+
 def after_build(source, target, env):
 	isExists=os.path.exists('build')
 	if not isExists:
 		os.mkdir('build')
-	shutil.copy(firmware_source, 'build/nano33ble.bin')
+	shutil.copy(firmware_source, 'build/arduino_serial_plotter.bin')
 
 env.AddPostAction("buildprog", after_build)
 
-firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware.bin")
